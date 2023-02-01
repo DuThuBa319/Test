608d7772fceb49353ce1f264522afc4313df6dbd
DuThuBa319 <khang.nguyentrong.sistrain@gmail.com>
2023-02-01 21:49:24 +0700

    add

--- lib/screens/Splash.dart
+++ lib/screens/Splash.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:git_test/screens/HomeScreen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    NavigateHome();
+  }
+
+  void NavigateHome() async {
+    await Future.delayed(const Duration(milliseconds: 1500));
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: ((context) => const HomeScreen())));
+  }
 
   @override
   Widget build(BuildContext context) {
